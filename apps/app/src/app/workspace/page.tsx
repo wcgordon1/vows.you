609feba1d25f08dbc6b8a3c4359a99ed4fd7e284
wcgordon1 <wcgordon1@gmail.com>
2026-02-13 10:11:49 -0500

    please deploy baby

--- apps/app/src/app/workspace/page.tsx
+++ apps/app/src/app/workspace/page.tsx
@@ -1,5 +1,7 @@
 import { WorkspaceLayout } from "./components/workspace-layout";
 
+export const dynamic = "force-dynamic";
+
 export default function WorkspacePage() {
   return <WorkspaceLayout />;
 }
